339516ed76ab44f02f23a05a943b804439d57e13
AgueroLucas <99370062+AgueroLucas@users.noreply.github.com>
2024-01-26 12:54:54 -0300

    cambio de imagenes

--- src/components/public/Card2.js
+++ src/components/public/Card2.js
@@ -16,6 +16,11 @@ const Card2 = () => {
     boxShadow: isHovered ? '0px 0px 10px rgba(0, 0, 0, 0.2)' : 'none',
   };
 
+  const imageContainerStyle = {
+    maxWidth: '100%',
+    maxHeight: '225px', // Ajusta esto según sea necesario
+  };
+
   return (
     <div className="col">
       <div
@@ -24,32 +29,37 @@ const Card2 = () => {
         onMouseLeave={handleMouseLeave}
         style={cardStyle}
       >
-        <svg
-          className="bd-placeholder-img card-img-top"
-          width="100%"
-          height="225"
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-label="Placeholder: Thumbnail"
-          preserveAspectRatio="xMidYMid slice"
-          focusable="false"
-        >
-          <title>Placeholder</title>
-          <rect width="100%" height="100%" fill="#55595c" />
-          <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-            
-          </text>
-        </svg>
+        <div className="image-container" style={imageContainerStyle}>
+          <svg
+            className="bd-placeholder-img card-img-top"
+            width="100%"
+            height="240"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: Thumbnail"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>curriculum</title>
+            <rect width="100%" height="100%" fill="#55595c" />
+            <text x="50%" y="50%" fill="#eceeef" dy=".3em"></text>
+            <image
+              href="https://i.pinimg.com/736x/7e/4c/21/7e4c212dc25e00d146d5dcfda5a3f059.jpg"
+              width="100%"
+              height="100%"
+              alt="curriculum"
+            />
+          </svg>
+        </div>
 
         <div className="card-body">
-          <p className="card-text">Proximamente</p>
+          <p className="card-text" style={{ fontFamily: 'Kanit', fontWeight: 'lighter' }}>Plantilla CV : Plataforma interactiva de currículum vitae que lleva la presentación de mis conocimientos al momento de ponerlos en practica..</p>
           <div className="d-flex justify-content-between align-items-center">
             <div className="btn-group">
-              <button type="button" className="btn btn-sm btn-outline-secondary">
-                Ir a sitio
-              </button>
+              <a href="https://curriculumestandar.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <button type="button" className="btn btn-sm btn-outline-secondary" style={{ fontFamily: 'Kanit', fontWeight: 'lighter' }}>Ir a sitio</button>
+              </a>
             </div>
-            <small className="text-muted"></small>
           </div>
         </div>
       </div>
--- src/components/public/Card3.js
+++ src/components/public/Card3.js
@@ -16,6 +16,11 @@ const Card3 = () => {
     boxShadow: isHovered ? '0px 0px 10px rgba(0, 0, 0, 0.2)' : 'none',
   };
 
+  const imageContainerStyle = {
+    maxWidth: '100%',
+    maxHeight: '225px', // Ajusta esto según sea necesario
+  };
+
   return (
     <div className="col">
       <div
@@ -24,32 +29,37 @@ const Card3 = () => {
         onMouseLeave={handleMouseLeave}
         style={cardStyle}
       >
-        <svg
-          className="bd-placeholder-img card-img-top"
-          width="100%"
-          height="225"
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-label="Placeholder: Thumbnail"
-          preserveAspectRatio="xMidYMid slice"
-          focusable="false"
-        >
-          <title>Placeholder</title>
-          <rect width="100%" height="100%" fill="#55595c" />
-          <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-            
-          </text>
-        </svg>
+        <div className="image-container" style={imageContainerStyle}>
+          <svg
+            className="bd-placeholder-img card-img-top"
+            width="100%"
+            height="240"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: Thumbnail"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>influencer</title>
+            <rect width="100%" height="100%" fill="#55595c" />
+            <text x="50%" y="50%" fill="#eceeef" dy=".3em"></text>
+            <image
+              href="https://i.pinimg.com/originals/b0/60/69/b06069f2090690b184760476ebeab5d1.png"
+              width="100%"
+              height="100%"
+              alt="ingluencer"
+            />
+          </svg>
+        </div>
 
         <div className="card-body">
-          <p className="card-text">Proximamente</p>
+          <p className="card-text" style={{ fontFamily: 'Kanit', fontWeight: 'lighter' }}>Plantilla Influencer : Plantilla diseñada para quienes buscan destacar en el mundo digital! Creada para que puedas mostrar tus plataformas favoritas.</p>
           <div className="d-flex justify-content-between align-items-center">
             <div className="btn-group">
-              <button type="button" className="btn btn-sm btn-outline-secondary">
-                Ir a sitio
-              </button>
+              <a href="https://influencerxperfil.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <button type="button" className="btn btn-sm btn-outline-secondary" style={{ fontFamily: 'Kanit', fontWeight: 'lighter' }}>Ir a sitio</button>
+              </a>
             </div>
-            <small className="text-muted"></small>
           </div>
         </div>
       </div>
